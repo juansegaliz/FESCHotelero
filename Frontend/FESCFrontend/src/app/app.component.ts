f9dbb8751630a833568063c69a5dc6986a4ca144
Thomas Rodriguez <thomasmauricio521@gmail.com>
2024-03-22 19:09:36 -0500

    rename project frontend

--- Frontend/FESCFrontend/src/app/app.component.ts
+++ Frontend/FESCFrontend/src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'FRF-root',
+  selector: 'Ffr-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'FESCRestoriaFrontend';
+  title = 'FESCFrontend';
 }
