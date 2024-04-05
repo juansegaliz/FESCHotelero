2a94d34d88cc0458921f908472b3374b90dbcf00
unknown <jugada160324@gmail.com>
2024-04-05 17:35:15 -0500

    Fix: Solucion de dependencias del proyecto para mejoras del rendimiento

---
+++ Mobile version/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'pages',
+    loadChildren: () =>
+      import('./pages/pages.module').then((m) => m.PagesPageModule),
+  },
+  {
+    path: '',
+    redirectTo: 'pages',
+    pathMatch: 'full',
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
