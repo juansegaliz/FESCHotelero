5f755f761a0c18a8e9139ac0e565d297b519cd73
Thomas Rodriguez <thomasmauricio521@gmail.com>
2024-04-16 18:07:08 -0500

    formulario login y limpieza de archivos

--- Frontend/FESCFrontend/src/app/pages/auth/auth.component.ts
+++ Frontend/FESCFrontend/src/app/pages/auth/auth.component.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { LoginFormComponent } from '../../components/login-form/login-form.component';
-import { FormularioLoginComponent } from '../../../components/formulario-login/formulario-login.component';
+import { FormularioLoginComponent } from '../../components/formulario-login/formulario-login.component';
 
 @Component({
   selector: 'Ffr-auth',
   standalone: true,
-  imports: [CommonModule, LoginFormComponent,FormularioLoginComponent],
+  imports: [CommonModule,FormularioLoginComponent],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss',
 })
