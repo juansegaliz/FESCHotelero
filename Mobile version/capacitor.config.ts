d6f897f21b8a445fd0c9a623184ced8262e7d74b
unknown <jugada160324@gmail.com>
2024-04-18 17:08:51 -0500

    Refactor: Se consigue implementar capacitor al proyecto

---
+++ Mobile version/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.AppHotelero',
+  appName: 'AppHotelero',
+  webDir: 'www'
+};
+
+export default config;
