cc95a1a88b08f74f71eb90d9ebe94a9643c59fd7
juansegaliz <juansegaliz@gmail.com>
2024-05-01 11:54:50 -0500

    Backend, sql para crear la tabla paises / countries

---
+++ Backend/BackendApiService/Infrastructure/Data/DBScripts/003 CountriesCreateTable.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE `fesc_hotelero`.`Countries` (
+  `CountryId` INT NOT NULL,
+  `Name` VARCHAR(100) NOT NULL,
+  `Code` CHAR(3) NOT NULL,
+  `CreatedAt` DATETIME NOT NULL,
+  `UpdatedAt` DATETIME NOT NULL,
+  `CreatedByUserId` INT NOT NULL,
+  `UpdatedByUserId` INT NOT NULL,
+  `StatusId` INT NOT NULL,
+  PRIMARY KEY (`CountryId`));
