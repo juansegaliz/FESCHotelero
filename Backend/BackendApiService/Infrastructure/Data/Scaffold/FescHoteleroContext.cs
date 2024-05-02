743a3a5000d96337bec135421258d4ec1f8022d1
juansegaliz <juansegaliz@gmail.com>
2024-05-02 08:27:38 -0500

    Backend, se actualiza scaffold modelos C# iguales a la base de datos

--- Backend/BackendApiService/Infrastructure/Data/Scaffold/FescHoteleroContext.cs
+++ Backend/BackendApiService/Infrastructure/Data/Scaffold/FescHoteleroContext.cs
@@ -15,10 +15,132 @@ public partial class FescHoteleroContext : DbContext
     {
     }
 
+    public virtual DbSet<Agency> Agencies { get; set; }
+
+    public virtual DbSet<CancellationReason> CancellationReasons { get; set; }
+
+    public virtual DbSet<City> Cities { get; set; }
+
+    public virtual DbSet<Company> Companies { get; set; }
+
+    public virtual DbSet<Country> Countries { get; set; }
+
+    public virtual DbSet<Guest> Guests { get; set; }
+
+    public virtual DbSet<Hotel> Hotels { get; set; }
+
+    public virtual DbSet<HotelEvent> HotelEvents { get; set; }
+
     public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySQL("server=localhost;database=fesc_hotelero;user=root;password=Root-Password-1234-$;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Agency>(entity =>
+        {
+            entity.HasKey(e => e.Agencyid).HasName("PRIMARY");
+
+            entity.Property(e => e.Address).HasMaxLength(250);
+            entity.Property(e => e.CreatedAt).HasColumnType("datetime");
+            entity.Property(e => e.Description).HasColumnType("text");
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.Name).HasMaxLength(250);
+            entity.Property(e => e.Phone).HasMaxLength(20);
+            entity.Property(e => e.UpdatedAt).HasColumnType("datetime");
+            entity.Property(e => e.Zip)
+                .HasMaxLength(20)
+                .HasColumnName("ZIP");
+        });
+
+        modelBuilder.Entity<CancellationReason>(entity =>
+        {
+            entity.HasKey(e => e.CancellationId).HasName("PRIMARY");
+
+            entity.Property(e => e.CreatedAt).HasColumnType("datetime");
+            entity.Property(e => e.Description).HasColumnType("text");
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.UpdatedAt).HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<City>(entity =>
+        {
+            entity.HasKey(e => e.CityId).HasName("PRIMARY");
+
+            entity.Property(e => e.CreatedAt).HasColumnType("datetime");
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.UpdatedAt).HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<Company>(entity =>
+        {
+            entity.HasKey(e => e.CompanyId).HasName("PRIMARY");
+
+            entity.Property(e => e.Address).HasMaxLength(250);
+            entity.Property(e => e.CreatedAt).HasColumnType("datetime");
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.Name).HasMaxLength(250);
+            entity.Property(e => e.Phone).HasMaxLength(20);
+            entity.Property(e => e.UpdatedAt).HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<Country>(entity =>
+        {
+            entity.HasKey(e => e.CountryId).HasName("PRIMARY");
+
+            entity.Property(e => e.Code)
+                .HasMaxLength(3)
+                .IsFixedLength();
+            entity.Property(e => e.CreatedAt).HasColumnType("datetime");
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.UpdatedAt).HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<Guest>(entity =>
+        {
+            entity.HasKey(e => e.GuestId).HasName("PRIMARY");
+
+            entity.HasIndex(e => e.Email, "Email_UNIQUE").IsUnique();
+
+            entity.Property(e => e.Address).HasMaxLength(200);
+            entity.Property(e => e.CreatedAt).HasColumnType("datetime");
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.Lastname).HasMaxLength(100);
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.Phone).HasMaxLength(20);
+            entity.Property(e => e.UpdatedAt).HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<Hotel>(entity =>
+        {
+            entity.HasKey(e => e.HotelId).HasName("PRIMARY");
+
+            entity.Property(e => e.Address).HasMaxLength(250);
+            entity.Property(e => e.CreatedAt).HasColumnType("datetime");
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.Name).HasMaxLength(250);
+            entity.Property(e => e.Phone).HasMaxLength(20);
+            entity.Property(e => e.UpdatedAt).HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<HotelEvent>(entity =>
+        {
+            entity.HasKey(e => e.HotelEventId).HasName("PRIMARY");
+
+            entity.Property(e => e.CreatedAt).HasColumnType("datetime");
+            entity.Property(e => e.Description).HasColumnType("text");
+            entity.Property(e => e.EndDate).HasColumnType("date");
+            entity.Property(e => e.EndTime).HasColumnType("time");
+            entity.Property(e => e.Location).HasMaxLength(100);
+            entity.Property(e => e.Name).HasMaxLength(150);
+            entity.Property(e => e.Price).HasPrecision(10);
+            entity.Property(e => e.StartDate).HasColumnType("date");
+            entity.Property(e => e.StartTime).HasColumnType("time");
+            entity.Property(e => e.UpdatedAt).HasColumnType("datetime");
+        });
+
         modelBuilder.Entity<User>(entity =>
         {
             entity.HasKey(e => e.UserId).HasName("PRIMARY");
---
+++ Backend/BackendApiService/Infrastructure/Data/Scaffold/HotelEvent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Infrastructure.Data.Scaffold;
+
+public partial class HotelEvent
+{
+    public int HotelEventId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public DateTime StartDate { get; set; }
+
+    public DateTime EndDate { get; set; }
+
+    public TimeSpan? StartTime { get; set; }
+
+    public TimeSpan? EndTime { get; set; }
+
+    public string? Location { get; set; }
+
+    public int? Capacity { get; set; }
+
+    public decimal? Price { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public int CreatedByUserId { get; set; }
+
+    public int UpdatedByUserId { get; set; }
+
+    public int StatusId { get; set; }
+
+    public int HotelId { get; set; }
+}
